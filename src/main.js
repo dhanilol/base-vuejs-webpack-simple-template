5c633965c1050db817fee572495aae884c9753d7
DESKTOP-0JE9SRD\danilol <dhanilol@bitbucket.org>
2018-07-13 16:30:32 -0300

    Added Bootstrap + minor config

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import Vue from 'vue'
 
+require('style-loader!css-loader!bootstrap/dist/css/bootstrap.min.css');
+require('bootstrap');
+
+
 new Vue({
-  el: '#app'
+    el: '#app',
+    data: {
+
+    }
 })
